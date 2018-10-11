b701e78d1d5a3074390cb926cf9b69e8eaa29ad4
Kkartik2501 <kumar.kartik25@gmail.com>
2018-10-12 01:44:02 +0530

    'second'

---
+++ learning_users/basic_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget= forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','password','email')
+
+class UserProfileInfoForm(forms.ModelForm):
+    # portfolio = forms.URLField(required=False)
+    # picture = forms.ImageField(required = False)
+    class Meta():
+        model = UserProfileInfo
+        # fields = '__all__'
+        fields = ('portfolio_site', 'profile_pic')
